96b4d8ed0865f4705224c502b924bc31432cf77d
silardg <gproduct1379@yahoo.com>
2018-04-05 11:31:47 +0200

    Working with software serial

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,54 +1,56 @@
 #include <DynamixelSerial.h>
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include "PacketSerial.h"
-#include "Encoding/COBS.h"
-#include "Encoding/SLIP.h"
-
-#define glavnaSSerial_RX           3
-#define glavnaSSerial_TX           7
-#define glavnaSSerial_Baud        9600
+#include <PacketSerial.h>
 
 #define actuatorSSerial_RX         10
 #define actuatorSSerial_TX         11
 #define actuatorSSerial_Baud       9600
 
-int x; uint8_t buf[3];
 
-PacketSerial GlavnaPacketSerial;
+#define Dynamixel_Baud        1000000
+#define DIRECTION_PIN        8
+
 PacketSerial ActuatorPacketSerial;
 
-SoftwareSerial glavnaSS(glavnaSSerial_RX, glavnaSSerial_TX);
 SoftwareSerial actuatorSS(actuatorSSerial_RX, actuatorSSerial_TX);
 
-void glavnaonPacketReceived(const uint8_t* buffer, size_t size)
+int x; uint8_t buf[3];
+
+void actuatoronPacketReceived(const uint8_t* buffer, size_t size)
 {
-    switch(buffer[0]) {
+  switch(buffer[1]) {
 
       /*
        * Dynamixel.move(ID, Position);
        */
       case 'm':
-        Dynamixel.move(buffer[1], ((buffer[2] << 8) | buffer[3]));
+        int calc;
+        calc = (int)((1024 / 255) * buffer[3]);
+        Dynamixel.move(buffer[2], calc);
+
         break;
 
       /*
        * Dynamixel.moveSpeed(ID, Position, Speed);
        */
       case 's':
-        Dynamixel.moveSpeed(buffer[1], ((buffer[2] << 8) | buffer[3]), ((buffer[4] << 8) | buffer[5]));
+        int calc_angle, calc_speed;
+        calc_angle = (int)((1024 / 255) * buffer[3]);
+        calc_speed = (int)((1024 / 255) * buffer[4]);
+        Dynamixel.moveSpeed(buffer[2], calc_angle, calc_speed);
         break;
 
       /*
        * Dynamixel.moving(ID)
        */
       case 'M':
-        x = Dynamixel.moving(buffer[1]);
+        x = Dynamixel.moving(buffer[2]);
 
         buf[0] = buffer[0];
-        buf[1] = x >> 8;
-        buf[2] = x & 0xFF;
-        GlavnaPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
+        buf[1] = buffer[1];
+        buf[2] = x;
+        ActuatorPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
 
         break;
 
@@ -56,12 +58,12 @@ void glavnaonPacketReceived(const uint8_t* buffer, size_t size)
        * Dynamixel.readPosition(ID);
        */
       case 'p':
-        x = Dynamixel.readPosition(buffer[1]);
+        x = Dynamixel.readPosition(buffer[2]);
 
-        buf[0] = buffer[0];
+        buf[0] = buffer[1];
         buf[1] = x >> 8;
         buf[2] = x & 0xFF;
-        GlavnaPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
+        ActuatorPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
 
         break;
 
@@ -69,12 +71,12 @@ void glavnaonPacketReceived(const uint8_t* buffer, size_t size)
        * Dynamixel.readVoltage(ID)
        */
       case 'v':
-        x = Dynamixel.readVoltage(buffer[1]);
+        x = Dynamixel.readVoltage(buffer[2]);
 
-        buf[0] = buffer[0];
+        buf[0] = buffer[1];
         buf[1] = x >> 8;
         buf[2] = x & 0xFF;
-        GlavnaPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
+        ActuatorPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
 
         break;
 
@@ -82,59 +84,32 @@ void glavnaonPacketReceived(const uint8_t* buffer, size_t size)
        * Dynamixel.readTemparature
        */
       case 't':
-        x = Dynamixel.readTemperature(buffer[1]);
+        x = Dynamixel.readTemperature(buffer[2]);
 
-        buf[0] = buffer[0];
+        buf[0] = buffer[1];
         buf[1] = x >> 8;
         buf[2] = x & 0xFF;
-        GlavnaPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
+        ActuatorPacketSerial.send(buf, sizeof(buf)/sizeof(buf)[0]);
 
         break;
 
-      /*
-       * Forwarding to actuator
-       */
-      case 'a':
-        // declare actuator buffer with -1 size
-        size_t act_size = size - 1;
-        uint8_t act_buffer[act_size];
 
-        /* Shift bytes one position back */
-        for(int i = size - 1; i > 0; i--) {
-          act_buffer[i] = buffer[i - 1];
-        }
-
-        // send to actuator
-        ActuatorPacketSerial.send(act_buffer, act_size);
-        break;
     }
-
-}
-
-void actuatoronPacketReceived(const uint8_t* buffer, size_t size)
-{
-    // direct forward
-    GlavnaPacketSerial.send(buffer, size);
 }
 
 void setup() {
-    /* GLAVNA PACKET SERIAL SETUP */
-    glavnaSS.begin(glavnaSSerial_Baud);
-    GlavnaPacketSerial.begin(glavnaSSerial_Baud);
-    GlavnaPacketSerial.setStream(&glavnaSS);
-    GlavnaPacketSerial.setPacketHandler(&glavnaonPacketReceived);
+    Dynamixel.begin(Dynamixel_Baud, DIRECTION_PIN);
+
+    Dynamixel.move(10, Dynamixel.readPosition(10));
+    Dynamixel.move(4, Dynamixel.readPosition(4));
 
     /* ACTUATOR PACKET SERIAL SETUP */
     actuatorSS.begin(actuatorSSerial_Baud);
-    ActuatorPacketSerial.begin(actuatorSSerial_Baud);
     ActuatorPacketSerial.setStream(&actuatorSS);
     ActuatorPacketSerial.setPacketHandler(&actuatoronPacketReceived);
-
-    delay(50);
 
 }
 
 void loop() {
-  GlavnaPacketSerial.update();
-  ActuatorPacketSerial.update();
+    ActuatorPacketSerial.update();
 }
